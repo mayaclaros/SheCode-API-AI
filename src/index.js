1ca13152831c8c9cabd2793ac5990e074eb4ab0c
mayaclaros <123900092+mayaclaros@users.noreply.github.com>
2024-08-08 15:07:26 -0400

    Axios
    added Axios to HTML and JS

---
+++ src/index.js
@@ -0,0 +1,10 @@
+function showAnswer(response) {
+  console.log(response);
+}
+
+let apiKey = "24b9b3ae0f6ea728ao45f6et261b0962";
+let context = "be polite and and provide a very short answer";
+let prompt = "When was the first AI created?";
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=Q${apiKey}`;
+
+axios.get(apiUrl).then(showAnswer);
